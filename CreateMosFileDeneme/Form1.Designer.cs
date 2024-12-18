f5a859778e17723961fa1b17256a4f3fd2faef4b
sedefelms <127693967+sedefelms@users.noreply.github.com>
2024-12-18 13:08:59 +0300

    Files uploaded.

---
+++ CreateMosFileDeneme/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace CreateMosFileDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExcelOku = new System.Windows.Forms.Button();
+            this.buttonDosyaOlustur = new System.Windows.Forms.Button();
+            this.buttonMosOlustur = new System.Windows.Forms.Button();
+            this.listBoxIslemler = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonExcelOku
+            // 
+            this.buttonExcelOku.Location = new System.Drawing.Point(12, 12);
+            this.buttonExcelOku.Name = "buttonExcelOku";
+            this.buttonExcelOku.Size = new System.Drawing.Size(80, 80);
+            this.buttonExcelOku.TabIndex = 0;
+            this.buttonExcelOku.Text = "Excel Oku";
+            this.buttonExcelOku.UseVisualStyleBackColor = true;
+            this.buttonExcelOku.Click += new System.EventHandler(this.buttonExcelOku_Click);
+            // 
+            // buttonDosyaOlustur
+            // 
+            this.buttonDosyaOlustur.Enabled = false;
+            this.buttonDosyaOlustur.Location = new System.Drawing.Point(12, 98);
+            this.buttonDosyaOlustur.Name = "buttonDosyaOlustur";
+            this.buttonDosyaOlustur.Size = new System.Drawing.Size(80, 80);
+            this.buttonDosyaOlustur.TabIndex = 1;
+            this.buttonDosyaOlustur.Text = "Dosya Oluştur";
+            this.buttonDosyaOlustur.UseVisualStyleBackColor = true;
+            this.buttonDosyaOlustur.Click += new System.EventHandler(this.buttonDosyaOlustur_Click);
+            // 
+            // buttonMosOlustur
+            // 
+            this.buttonMosOlustur.Enabled = false;
+            this.buttonMosOlustur.Location = new System.Drawing.Point(12, 184);
+            this.buttonMosOlustur.Name = "buttonMosOlustur";
+            this.buttonMosOlustur.Size = new System.Drawing.Size(80, 80);
+            this.buttonMosOlustur.TabIndex = 2;
+            this.buttonMosOlustur.Text = ".mos Oluştur";
+            this.buttonMosOlustur.UseVisualStyleBackColor = true;
+            this.buttonMosOlustur.Click += new System.EventHandler(this.buttonMosOlustur_Click);
+            // 
+            // listBoxIslemler
+            // 
+            this.listBoxIslemler.FormattingEnabled = true;
+            this.listBoxIslemler.ItemHeight = 16;
+            this.listBoxIslemler.Location = new System.Drawing.Point(114, 20);
+            this.listBoxIslemler.Name = "listBoxIslemler";
+            this.listBoxIslemler.Size = new System.Drawing.Size(264, 244);
+            this.listBoxIslemler.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 279);
+            this.Controls.Add(this.listBoxIslemler);
+            this.Controls.Add(this.buttonMosOlustur);
+            this.Controls.Add(this.buttonDosyaOlustur);
+            this.Controls.Add(this.buttonExcelOku);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExcelOku;
+        private System.Windows.Forms.Button buttonDosyaOlustur;
+        private System.Windows.Forms.Button buttonMosOlustur;
+        private System.Windows.Forms.ListBox listBoxIslemler;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ CreateMosFileDeneme/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CreateMosFileDeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[][] lists;
+        string projectFolderPath = "C:\\Proje Dosyaları\\";
+        string nodeFolderPath = string.Empty;
+
+        private void buttonExcelOku_Click(object sender, EventArgs e)
+        {
+
+            if(listBoxIslemler.Items.Count != 0)
+            {
+                listBoxIslemler.Items.Clear();
+            }
+            string cellValue = null;
+            bool stop = false;
+            string excelFileName = null;
+            int index = 0;
+            do
+            {
+                string excelFilePath = string.Empty;
+                openFileDialog1.InitialDirectory = Application.StartupPath;
+                openFileDialog1.FileName = "*.xlsx";
+                openFileDialog1.Filter = "xlsx files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    excelFilePath = openFileDialog1.FileName;
+                    index = excelFilePath.LastIndexOf("\\") + 1;
+                    excelFileName = excelFilePath.Substring(index);
+                    MessageBox.Show(excelFileName + " dosyası seçildi.");
+                    listBoxIslemler.Items.Add(excelFileName + " dosyası seçildi.");
+                    Excel.Application xlApp = new Excel.Application();
+                    Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(excelFilePath);
+                    Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                    Excel.Range xlRange = xlWorksheet.UsedRange;
+                    int rowCount = xlRange.Rows.Count;
+                    int colCount = xlRange.Columns.Count;
+                    lists = new string[rowCount][];
+                    for (int i = 0; i <= rowCount - 1; i++)
+                    {
+                        lists[i] = new string[colCount];
+                        for (int j = 0; j <= colCount - 1; j++)
+                        {
+                            if (xlRange.Cells[i + 1, j + 1] != null && xlRange.Cells[i + 1, j + 1].Value2 != null)
+                            {
+                                cellValue = xlRange.Cells[i + 1, j + 1].Value2.ToString();
+                                lists[i][j] = cellValue;
+                            }
+                        }
+                    }
+                    listBoxIslemler.Items.Add("Excel dosyası okundu.");
+                    for (int i = 1; i < lists.Length; i++)
+                    {
+                        nodeFolderPath = projectFolderPath + lists[i][0];
+                        if (Directory.Exists(nodeFolderPath))
+                        {
+                            Directory.Delete(nodeFolderPath, true);
+                            MessageBox.Show("Mevcut olan " + lists[i][0] + " klasörü silindi.");
+                            listBoxIslemler.Items.Add("Mevcut olan " + lists[i][0] + " klasörü silindi.");
+                        }
+                    }
+                    buttonDosyaOlustur.Enabled = true;
+                    xlApp.Workbooks.Close();
+                    xlApp.Quit();
+
+                    xlRange = null;
+                    xlWorksheet = null;
+                    xlWorkbook = null;
+                    xlApp = null;
+                    stop = true;
+                }
+                else
+                {
+                    MessageBox.Show("Excel dosyasını seçmediniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+            }
+            while (stop == false);
+        }
+
+
+
+        private void buttonDosyaOlustur_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(projectFolderPath);
+            for (int i = 1; i < lists.Length; i++)
+            {
+                nodeFolderPath = projectFolderPath + lists[i][0];
+                Directory.CreateDirectory(nodeFolderPath);
+                listBoxIslemler.Items.Add(lists[i][0] + " klasörü oluşturuldu.");
+            }
+            buttonMosOlustur.Enabled = true;
+        }
+
+
+        string mosFileName = string.Empty;
+        string mosFilePath = string.Empty;
+        string value = string.Empty;
+
+        private void buttonMosOlustur_Click(object sender, EventArgs e)
+        {
+            string mosText = string.Empty;
+            for (int i = 1; i < lists.Length; i++)
+            {
+                for (int j = 1; j < lists[i].Length; j++)
+                {
+                    if(lists[i][j] == null)
+                    {
+                        lists[i][j] = "-";
+                    }
+                    else
+                    {
+                        if (lists[0][j].StartsWith("IPV6"))
+                        {
+                            string [] seperate = lists[i][j].Split(':');
+                            for (int k = 0; k < 8; k++)
+                            {
+                                if (seperate[k].StartsWith("0000"))
+                                {
+                                    value += ":";
+                                }
+                                else if (seperate[k].StartsWith("000"))
+                                {
+                                    value += seperate[k].Substring(3) + ":";
+                                }
+                                else if (seperate[k].StartsWith("00"))
+                                {
+                                    value += seperate[k].Substring(2) + ":";
+                                }
+                                else if (seperate[k].StartsWith("0"))
+                                {
+                                    value += seperate[k].Substring(1) + ":";
+                                }
+                                else
+                                {
+                                    value += seperate[k] + ":";
+                                }
+                            }
+                            while (value.Contains(":::"))
+                            {
+                                value = value.Replace(":::", "::");
+                            }
+                            lists[i][j] = value.Substring(0,value.Length-1);
+
+                            value = "";
+
+
+
+                            //for (int k = 0; k < lists[i][j].Length; k+=5)
+                            //{
+                            //    if (lists[i][j].Substring(k, 4) == "0000")
+                            //    {
+                            //        value = lists[i][j].Substring(k + 4, 1);
+                            //    }
+                            //    else if(lists[i][j].Substring(k, 4).StartsWith("000")){
+                            //        value = lists[i][j].Substring(k + 3, 2);
+                            //    }
+                            //    else if(lists[i][j].Substring(k, 4).StartsWith("00"))
+                            //    {
+                            //        value = lists[i][j].Substring(k + 2, 3);
+                            //    }
+                            //    else if(lists[i][j].Substring(k, 4).StartsWith("0"))
+                            //    {
+                            //        value = lists[i][j].Substring(k + 1, 4);
+                            //    }
+                            //    else
+                            //    {
+                            //        if (lists[i][j].Substring(k, 5).Length < 5)
+                            //        {
+                            //            value = lists[i][j].Substring(k, 4);
+                            //        }
+                            //        else
+                            //        {
+                            //            value = lists[i][j].Substring(k, 5);
+                            //        }
+                            //    }
+                            //    lists[i][j] += value;  
+                            //}
+
+                        }
+                    }
+                    mosText += lists[0][j] + " " + lists[i][j] + Environment.NewLine;
+
+                    //value = lists[i][j];
+                    //if(lists[i][j] == string.Empty)
+                    //{
+                    //    lists[i][j] = "-";
+                    //}
+                    //if (lists[0][j].StartsWith("IPV6") && lists[i][j] != "-")
+                    //{
+                    //    string[] eachValue = new string[8];
+                    //    for (int k = 0; k / 5 < eachValue.Length; k += 5)
+                    //    {
+                    //        eachValue[k / 5] = value.Substring(k, 4);
+                    //    }
+                    //    for (int l = 0; l < 8; l++)
+                    //    {
+                    //        if (eachValue[l].StartsWith("0"))
+                    //        {
+                    //            eachValue[l] = eachValue[l].Substring(1); 
+                    //            lists[i][j] += eachValue[l] + ":";
+                    //        }  
+                    //        lists[i][j] = lists[i][j].Substring(0, lists[i][j].Length - 1);                       
+                    //    }
+
+                    //    mosText += lists[0][j] + " " + lists[i][j] + Environment.NewLine;
+                    //}
+                    //else
+                    //{
+                    //    mosText += lists[0][j] + " " + lists[i][j] + Environment.NewLine;
+                    //}
+
+                }
+                nodeFolderPath = projectFolderPath + lists[i][0];
+                mosFileName = lists[i][0] + ".mos";
+                mosFilePath = Path.Combine(nodeFolderPath, mosFileName);
+                StreamWriter sw = new StreamWriter(mosFilePath);
+                sw.WriteLine(mosText);
+                sw.Close();
+                mosText = "";
+                listBoxIslemler.Items.Add(mosFileName + " dosyası oluşturuldu.");
+            }
+            listBoxIslemler.Items.Add("İşlem tamamlandı.");
+            if (buttonDosyaOlustur.Enabled || buttonMosOlustur.Enabled)
+            {
+                buttonDosyaOlustur.Enabled = false;
+                buttonMosOlustur.Enabled = false;
+            }
+        }
+    }
+}
